ba6feeeb14e72da9b10dc95bef3397e37feaabc0
lsf <lsf921016@126.com>
2017-04-04 15:54:30 -0400

    commit

--- src/test.java
+++ src/test.java
@@ -1,23 +1,45 @@
-import Algorithm.selection.Test;
+import sun.plugin.javascript.navig.Array;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * Created by lenovo on 2017/4/1.
+ * Created by lenovo on 2017/4/2.
  */
-class B{
-    class C{
-        private int a=2;
-        void print(){
-            System.out.println(this.a);
-        }
-    }
-    private int a=4;
-    void print(){
-        C cc=new C();
-        cc.print();
-        System.out.println(this.a);
-    }
+public class Test {
+    public static void main(String[] args) throws NoSuchMethodException {
+        //得到作用目标,类
+        Class clazz=A.class;
 
-    public static void main(String[] args) {
+        //得到作用目标，方法
+        Method method=clazz.getMethod("show");
+
+        //获取指定类型的注解
+        MyAnno myAnno1= (MyAnno) clazz.getAnnotation(MyAnno.class);
+        System.out.println(myAnno1.name()+","+myAnno1.age());
+
+        MyAnno myAnno2=method.getAnnotation(MyAnno.class);
+        System.out.println(myAnno2.name()+","+myAnno2.age());
+    }
+}
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno{
+    int age();
+    String name();
+}
+
+@MyAnno(age=1,name="lin")
+class A{
+
+    @MyAnno(age=2,name="li")
+    public void show(){
 
     }
 }
+
+
+
